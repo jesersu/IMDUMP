88ccb159e9dbadb1dfc847731eea6882eda2e15f
Jesus Chapi <jesersu@gmail.com>
2025-12-10 17:04:45 -0500

    Remove Firebase import to fix build - app works with mock data

--- IMDUMB/AppDelegate.swift
+++ IMDUMB/AppDelegate.swift
@@ -1,12 +1,12 @@
 import UIKit
-import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Configure Firebase
-        FirebaseApp.configure()
+        // Configure Firebase (optional - app works with mock data)
+        // Uncomment when Firebase SDK is added via SPM:
+        // FirebaseApp.configure()
 
         return true
     }
